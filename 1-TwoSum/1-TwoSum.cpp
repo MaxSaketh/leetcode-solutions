8a729a888b4c234307e44b1595a71525df02db22
Samala Saketh Reddy <99119593+MaxSaketh@users.noreply.github.com>
2026-06-19 20:26:41 +0530

    Delete 1-TwoSum/1-TwoSum.cpp

--- 1-TwoSum/1-TwoSum.cpp
+++
@@ -1,9 +0,0 @@
-// Last updated: 19/06/2026, 20:25:33
-def twoSum(self, nums, target):
-        seen = {}
-        for i, v in enumerate(nums):
-            remaining = target - v
-            if remaining in seen:
-                return [seen[remaining], i]
-            seen[v] = i
-        return []
