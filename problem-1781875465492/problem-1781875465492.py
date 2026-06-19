5748add868595687025c75e9ca3fac11425a7c1b
Samala Saketh Reddy <99119593+MaxSaketh@users.noreply.github.com>
2026-06-19 18:57:56 +0530

    Delete problem-1781875465492 directory
    Duplicate

--- problem-1781875465492/problem-1781875465492.py
+++
@@ -1,9 +0,0 @@
-# Last updated: 19/06/2026, 18:54:25
-1class Solution:
-2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        for indi, i in enumerate(nums):
-4            for indj, j in enumerate(nums):
-5                if i + j == target and indi != indj:
-6                    return [indi, indj]
-7
-8                    
