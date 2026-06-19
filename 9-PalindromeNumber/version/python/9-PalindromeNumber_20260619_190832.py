34751d6d6a019c987f57a9f4541a1b13ec738a4e
Samala Saketh Reddy <99119593+MaxSaketh@users.noreply.github.com>
2026-06-19 19:30:12 +0530

    Fix formatting and ensure consistent indentation

--- 9-PalindromeNumber/version/python/9-PalindromeNumber_20260619_190832.py
+++ 9-PalindromeNumber/version/python/9-PalindromeNumber_20260619_190832.py
@@ -1,5 +1,5 @@
 # Last updated: 19/06/2026, 19:08:32
-1# String method
-2class Solution:
-3    def isPalindrome(self, x: int) -> bool:
-4        return str(x)==str(x)[::-1]
+# String method
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x)==str(x)[::-1]
