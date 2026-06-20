6c88e636e3aec8f32c36c1c5601e95c07cf8cccd
Samala Saketh Reddy <99119593+MaxSaketh@users.noreply.github.com>
2026-06-20 19:28:29 +0530

    Create

---
+++ problem-1781963908748/version/python/problem-1781963908748_20260620_192828.py
@@ -0,0 +1,6 @@
+# Last updated: 20/06/2026, 19:28:28
+# mb, i just over thinked in prev submission 🫠
+1import numpy as np
+2class Solution:
+3    def getConcatenation(self, nums: List[int]) -> List[int]:
+4        return nums + nums
