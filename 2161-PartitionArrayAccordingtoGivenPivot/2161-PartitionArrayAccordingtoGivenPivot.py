73ccebfbc0b7588353ac131af155b392cda2c794
Samala Saketh Reddy <99119593+MaxSaketh@users.noreply.github.com>
2026-06-20 19:10:33 +0530

    Create 2161-PartitionArrayAccordingtoGivenPivot

---
+++ 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.py
@@ -0,0 +1,11 @@
+# Last updated: 20/06/2026, 19:10:33
+1class Solution:
+2    def finalValueAfterOperations(self, operations: List[str]) -> int:
+3        init = 0
+4
+5        for i in operations:
+6            if i in ["++X","X++"]:
+7                init += 1
+8            else: 
+9                init -= 1
+10        return init
